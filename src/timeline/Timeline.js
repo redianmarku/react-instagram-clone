ffaf0463f95a0142f26f893b5e58f65a15e1d883
Redian Marku <redian1marku@gmail.com>
2023-03-17 22:14:34 +0100

    Removed the stories

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Post from "./Post/Post";
 import Suggestions from "./Suggestions";
 import "./Timeline.css";
-import Stories from "react-insta-stories";
 
 function Timeline() {
   const [posts, setPosts] = useState([
@@ -39,12 +38,6 @@ function Timeline() {
   return (
     <div className="timeline">
       <div className="timeline__left">
-        {/* <Stories
-          stories={stories}
-          defaultInterval={1500}
-          width={432}
-          height={768}
-        /> */}
         <div className="timeline__posts">
           {posts.map((post) => (
             <Post
