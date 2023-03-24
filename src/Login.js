4f1a8a604129e49f130f085799e81e445e949b7f
Redian Marku <redian1marku@gmail.com>
2023-03-24 22:04:41 +0100

    working in signup component

--- src/Login.js
+++ src/Login.js
@@ -41,4 +41,38 @@ function Login() {
   );
 }
 
+const Sigup = () => {
+  return (
+    <div className="signup">
+      <div className="signup__form">
+        <img
+          src="https://assets.stickpng.com/images/5a4e432a2da5ad73df7efe7a.png"
+          alt=""
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Username"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button>Log in</button>
+      </div>
+      <div className="signup__more">
+        <span>
+          Don't have an account? <button>Sign Up</button>
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBZp1sjt--KhZ1B0iOaN-kRzfEV0HZTQwM",
-  authDomain: "instagram-clone-413fb.firebaseapp.com",
-  projectId: "instagram-clone-413fb",
-  storageBucket: "instagram-clone-413fb.appspot.com",
-  messagingSenderId: "51478448474",
-  appId: "1:51478448474:web:4b18b310ebf09e82e84b63",
-  measurementId: "G-HW4VRCPBVP",
+  apiKey: "AIzaSyCXyJAggdftc1y1gcUu-SWlgwe-TaThP_A",
+  authDomain: "instagram-clone-6fa4d.firebaseapp.com",
+  projectId: "instagram-clone-6fa4d",
+  storageBucket: "instagram-clone-6fa4d.appspot.com",
+  messagingSenderId: "717508443850",
+  appId: "1:717508443850:web:f9eee947387c9d1f82fe33",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth();
