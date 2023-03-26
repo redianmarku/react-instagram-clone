4aad663d6d7b4fbf34e8c36e9d401801a788b9aa
Redian Marku <redian1marku@gmail.com>
2023-03-27 00:13:19 +0200

    base code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 import "./App.css";
 import Homepage from "./Homepage";
-import Authenticate from "./authenticate/Authenticate";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { auth } from "./firebase";
-import { loginUser, setLoading } from "./features/userSlice";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        dispatch(
-          loginUser({
-            uid: authUser.uid,
-            username: authUser.displayName,
-            email: authUser.email,
-          })
-        );
-        dispatch(setLoading(false));
-      } else {
-        dispatch(setLoading(false));
-        console.log("User is not logged in.");
-      }
-    });
-  }, []);
-
-  const user = useSelector((state) => state.data.user.user);
-  const isLoading = useSelector((state) => state.data.user.isLoading);
   return (
     <div className="app">
-      {isLoading ? (
-        <div class="loader-container">
-          <div class="loader"></div>
-        </div>
-      ) : (
-        <>{user ? <Homepage /> : <Authenticate />}</>
-      )}
+      <Homepage />
     </div>
   );
 }
--- src/app/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "./reducers";
-
-export const store = configureStore({
-  reducer: { data: rootReducer },
-});
