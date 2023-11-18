eb8c4a615aa6982c79149dbaa904653df82eda79
sakshi <sakshirana765@gmail.com>
2023-11-18 19:20:00 +0530

    Instagram-clone

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
